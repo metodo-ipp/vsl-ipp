278d0c9320a0c8b4bf7ce89ec679ad9f0207d06d
rickbrumatti <rickbrumatti@icloud.com>
2026-07-24 21:48:19 -0300

    feat: atualiza VSL padrão das rotas CID

--- artifacts/escola-de-governo/src/pages/cid-bio.tsx
+++ artifacts/escola-de-governo/src/pages/cid-bio.tsx
@@ -6,8 +6,8 @@ export default function CidBio() {
     trackingPrefix="cid_bio"
     video={{
       type: "vturb",
-      playerId: "vid-6a6233971ac7c381ab3cd275",
-      playerScript: "https://scripts.converteai.net/d96d1452-17dc-48ff-8763-0c764e770de2/players/6a6233971ac7c381ab3cd275/v4/player.js",
+      playerId: "vid-6a64060629f07dc8b65d2443",
+      playerScript: "https://scripts.converteai.net/d96d1452-17dc-48ff-8763-0c764e770de2/players/6a64060629f07dc8b65d2443/v4/player.js",
     }}
   />;
 }
