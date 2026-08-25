9f8f2eea804a2665738e87723117362a063a600f
“samuelpereiraipp” <samperprotecaoveicular@gmail.com>
2026-08-24 22:17:12 -0300

    Update Manychat CID VSL player and pitch timing

--- artifacts/escola-de-governo/src/pages/cid-manychat.tsx
+++ artifacts/escola-de-governo/src/pages/cid-manychat.tsx
@@ -1,13 +1,18 @@
 import CidVslPage from "@/components/cid-vsl-page";
 
+const PITCH_DELAY_SECONDS = 9 * 60 + 10;
+
 export default function CidManychat() {
   return <CidVslPage
     checkoutUrl="https://hub.la/r/cid-vsl-manychat"
     trackingPrefix="cid_manychat"
+    videoAspectRatio="9 / 16"
+    videoMaxWidth={400}
+    pitchDelaySeconds={PITCH_DELAY_SECONDS}
     video={{
       type: "vturb",
-      playerId: "vid-6a64060629f07dc8b65d2443",
-      playerScript: "https://scripts.converteai.net/d96d1452-17dc-48ff-8763-0c764e770de2/players/6a64060629f07dc8b65d2443/v4/player.js",
+      playerId: "vid-6a892e63245921387e3bf113",
+      playerScript: "https://scripts.converteai.net/d96d1452-17dc-48ff-8763-0c764e770de2/players/6a892e63245921387e3bf113/v4/player.js",
     }}
   />;
 }
